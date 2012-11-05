b382ff27b1cc2bee7b154b8ad30e9de804f676b9
Martin Flores <martin.flores@bodybuilding.com>
2012-11-05 12:40:45 -0600

    Eliminando codigo no utilizado

---
+++ projects/mongodb-repository/src/test/java/com/bodybuilding/techtalk/repository/CustomerRepositoryTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.bodybuilding.techtalk.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author martin
+ *
+ */
+public class CustomerRepositoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
